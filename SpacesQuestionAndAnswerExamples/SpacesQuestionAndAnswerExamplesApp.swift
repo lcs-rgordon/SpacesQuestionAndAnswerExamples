517ef12be190dcbfdd84039695df8e414106ff26
Russell Gordon <rgordon@lcs.on.ca>
2023-01-13 13:43:55 -0500

    Initial Commit

---
+++ SpacesQuestionAndAnswerExamples/SpacesQuestionAndAnswerExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpacesQuestionAndAnswerExamplesApp.swift
+//  SpacesQuestionAndAnswerExamples
+//
+//  Created by Russell Gordon on 2023-01-13.
+//
+
+import SwiftUI
+
+@main
+struct SpacesQuestionAndAnswerExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
